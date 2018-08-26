c37a092557d508ffd4c0e8999e361669096ce059
hmallen <allenhm@gmail.com>
2018-08-26 18:20:55 -0500

    Testing readlines.

--- python/repeater.py
+++ python/repeater.py
@@ -9,8 +9,8 @@ logger.setLevel(logging.DEBUG)
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
-    baudrate=9600#,
-    #timeout=1
+    baudrate=9600,
+    timeout=0.5
 )
 
 
@@ -18,12 +18,13 @@ if __name__ == '__main__':
     while (True):
         if ser.in_waiting > 0:
             # Receive, decode, and strip trailing \r\n from incoming message
-            msg = ser.readline()
+            #msg = ser.readline()
+            msg = ser.readlines()
             msg_decoded = msg.decode().rstrip('\r\n')
             logger.debug('msg_decoded: ' + msg_decoded)
 
             # Rebroadcast message for remote units (repeater function)
             ser.write(msg)
-            logger.debug('Rebroadcasted received message.')
+            logger.debug('Rebroadcasted message.')
 
         time.sleep(0.01)
