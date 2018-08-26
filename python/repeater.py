4db8a3941564d8ee6f2027b54d75879c8a31a817
hmallen <allenhm@gmail.com>
2018-08-26 10:33:21 -0500

    Working on serial read.

--- python/repeater.py
+++ python/repeater.py
@@ -18,12 +18,12 @@ if __name__ == '__main__':
     while (True):
         if ser.in_waiting > 0:
             # Receive, decode, and strip trailing \r\n from incoming message
-            msg = ser.read()
+            msg = ser.readline()
             msg_decoded = msg.decode().rstrip('\r\n')
             logger.debug('msg_decoded: ' + msg_decoded)
 
             # Rebroadcast message for remote units (repeater function)
-            ser.write(msg_decoded.encode('utf-8'))
+            ser.write(msg)
             logger.debug('Rebroadcasted received message.')
 
         time.sleep(0.01)
