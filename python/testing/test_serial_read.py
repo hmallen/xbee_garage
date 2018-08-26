ef1ec745ed034dc820b879f4aee7f361b05f67d0
hmallen <allenhm@gmail.com>
2018-08-25 20:41:20 -0500

    Added sleep to reduce CPU usage.

--- python/testing/test_serial_read.py
+++ python/testing/test_serial_read.py
@@ -1,6 +1,7 @@
 import logging
 import serial
 import sys
+import time
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
@@ -13,3 +14,6 @@ if __name__ == '__main__':
     while (True):
         if ser.in_waiting > 0:
             msg = ser.read()
+            print(msg)
+
+        time.sleep(0.01)
