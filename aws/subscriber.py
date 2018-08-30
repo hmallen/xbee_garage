e1ea66790e43659f1130d608fa47e384f4837253
hmallen <allenhm@gmail.com>
2018-08-29 22:37:59 -0500

    Fixed subscriber callback functions.

--- aws/subscriber.py
+++ aws/subscriber.py
@@ -12,49 +12,49 @@ logger.setLevel(logging.DEBUG)
 
 config_path = '../config/config_aws.ini'
 
-config = configparser.ConfigParser()
-config.read(config_path)
-
-awshost = config['awsiot']['awshost']
-awsport = int(config['awsiot']['awsport'])
-clientId = config['awsiot']['clientId']
-thingName = config['awsiot']['thingName']
-caPath = config['certificates']['caPath']
-certPath = config['certificates']['certPath']
-keyPath = config['certificates']['keyPath']
-
-connflag = False
-
 
 def on_connect(client, userdata, flags, rc):
-    global connflag
-    connflag = True
-    print("Connection returned result: " + str(rc))
+    print("Connection returned result: " + str(rc) )
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("#" , 1 )
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    print("topic: " + msg.topic)
+    print("payload: " + str(msg.payload))
 
 
 # def on_log(client, userdata, level, buf):
     # print(msg.topic + " " + str(msg.payload))
 
 
-#if __name__ == '__main__':
-mqttc = paho.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-# mqttc.on_log = on_log
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read(config_path)
 
-mqttc.tls_set(
-    caPath,
-    certfile=certPath,
-    keyfile=keyPath,
-    cert_reqs=ssl.CERT_REQUIRED,
-    tls_version=ssl.PROTOCOL_TLSv1_2,
-    ciphers=None
-)
+    awshost = config['awsiot']['awshost']
+    awsport = int(config['awsiot']['awsport'])
+    clientId = config['awsiot']['clientId']
+    thingName = config['awsiot']['thingName']
+    caPath = config['certificates']['caPath']
+    certPath = config['certificates']['certPath']
+    keyPath = config['certificates']['keyPath']
+    
+    mqttc = paho.Client()
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    # mqttc.on_log = on_log
 
-mqttc.connect(awshost, awsport, keepalive=60)
+    mqttc.tls_set(
+        caPath,
+        certfile=certPath,
+        keyfile=keyPath,
+        cert_reqs=ssl.CERT_REQUIRED,
+        tls_version=ssl.PROTOCOL_TLSv1_2,
+        ciphers=None
+    )
 
-mqttc.loop_forever()
+    mqttc.connect(awshost, awsport, keepalive=60)
+
+    mqttc.loop_forever()
