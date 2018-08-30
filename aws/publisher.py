ecfed42eb8b93480b96e028c93b1c8cb8884054f
hmallen <allenhm@gmail.com>
2018-08-29 22:19:10 -0500

    Added config parsing.

--- aws/publisher.py
+++ aws/publisher.py
@@ -1,65 +1,70 @@
-#!/usr/bin/python
-
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
-
-# use this program to test the AWS IoT certificates received by the author
-# to participate to the spectrogram sharing initiative on AWS cloud
-
-# this program will publish test mqtt messages using the AWS IoT hub
-# to test this program you have to run first its companion awsiotsub.py
-# that will subscribe and show all the messages sent by this program
-
-import paho.mqtt.client as paho
+import configparser
+import logging
 import os
 import socket
 import ssl
-from time import sleep
-from random import uniform
+import time
+
+import paho.mqtt.client as paho
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+config_path = '../config/config_aws.ini'
 
 connflag = False
+
 
 def on_connect(client, userdata, flags, rc):
     global connflag
     connflag = True
-    print("Connection returned result: " + str(rc) )
+    print("Connection returned result: " + str(rc))
+
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
+
 
 # def on_log(client, userdata, level, buf):
-    # print(msg.topic+" "+str(msg.payload))
+    # print(msg.topic + " " + str(msg.payload))
 
-mqttc = paho.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-# mqttc.on_log = on_log
 
-awshost = "a1oqi4ny53myjs.iot.us-east-1.amazonaws.com"
-awsport = 8883
-clientId = "xbeeGarage"
-thingName = "xbeeGarage"
-caPath = "/home/pi/xbee_garage/aws/certs/aws-iot-rootCA.crt"
-certPath = "/home/pi/xbee_garage/aws/certs/cert.pem"
-keyPath = "/home/pi/xbee_garage/aws/certs/privateKey.pem"
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read(config_path)
 
-mqttc.tls_set(
-    caPath,
-    certfile=certPath,
-    keyfile=keyPath,
-    cert_reqs=ssl.CERT_REQUIRED,
-    tls_version=ssl.PROTOCOL_TLSv1_2,
-    ciphers=None
-)
+    awshost = config['awsiot']['awshost']
+    awsport = config['awsiot']['awsport']
+    clientId = config['awsiot']['clientId']
+    thingName = config['awsiot']['thingName']
+    caPath = config['certificates']['caPath']
+    certPath = config['certificates']['certPath']
+    keyPath = config['certificates']['keyPath']
 
-mqttc.connect(awshost, awsport, keepalive=60)
+    mqttc = paho.Client()
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    # mqttc.on_log = on_log
 
-mqttc.loop_start()
+    mqttc.tls_set(
+        caPath,
+        certfile=certPath,
+        keyfile=keyPath,
+        cert_reqs=ssl.CERT_REQUIRED,
+        tls_version=ssl.PROTOCOL_TLSv1_2,
+        ciphers=None
+    )
 
-while (True):
-    sleep(0.5)
-    if connflag == True:
-        tempreading = uniform(20.0,25.0)
-        mqttc.publish("temperature", tempreading, qos=1)
-        print("msg sent: temperature " + "%.2f" % tempreading )
-    else:
-        print("waiting for connection...")
+    mqttc.connect(awshost, awsport, keepalive=60)
+
+    mqttc.loop_start()
+
+    while (True):
+        time.sleep(0.5)
+        if connflag == True:
+            tempreading = uniform(20.0,25.0)
+            mqttc.publish("temperature", tempreading, qos=1)
+            print("msg sent: temperature " + "%.2f" % tempreading )
+        else:
+            print("waiting for connection...")
