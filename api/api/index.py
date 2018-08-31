31eba7658df3b50ddb21f1ef3d7e797240eae8db
hmallen <allenhm@gmail.com>
2018-08-31 00:50:40 -0500

    Testing new endpoints.

--- api/api/index.py
+++ api/api/index.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def home():
+    return 'All hail Gene Sundeen!'
+
+@app.route('/sensors')
+def list_sensors():
+    sensors = [
+        'door',
+        'carbon-monoxide',
+        'air-quality'
+    ]
+    return jsonify(sensors)
+
+@app.route('/locks')
+def list_locks():
+    locks = [
+        'door',
+        'button'
+    ]
+    return jsonify(locks)
